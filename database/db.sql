f87c167bf9297d1a3ae605370ed134847515c460
Daniel Cifuentes <54119708+DanielCifuentes05@users.noreply.github.com>
2020-10-03 21:34:07 -0500

    database conf
    configuración database

---
+++ database/db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db_supportbike;
+
+USE db_supportbike;
+
+CREATE TABLE user(
+    id INT(11) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    email VARCHAR(100) NOT NULL
+
+);
+
---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports= {
+    database: {
+        host:'localhost:3306',
+        user: 'root',
+        password : '',
+        database: 'db_supportbike'
+    }
+};
