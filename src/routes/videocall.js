66b5cdf0c8c489ec806d69fdc68068102bf539e5
Daniel Cifuentes <54119708+DanielCifuentes05@users.noreply.github.com>
2020-11-26 16:41:47 -0500

    Integrando Jistsi
    Añadiendo jisti como api de videollamadas

--- src/routes/videocall.js
+++ src/routes/videocall.js
@@ -7,24 +7,6 @@ router.get('/videocall' , isLoggedIn, (req, res) => {
     res.render('videocall/videocall');
 });
 
-router.post('/videocall/auth', (req, res) => {
 
-    const pusher = new Pusher({
-        appId: '1089719',
-        key: 'aa627cb1dc3099475e48',
-        secret: 'a2d65af256706949f73b',
-        cluster: 'us2',
-        useTLS: true
-    });
-
-    const socketId = req.body.socket_id;
-    const channel = req.body.channel_name;
-    var presenceData = {
-        user_id: req.user.nombre + "-" + Math.random().toString(14).slice(2) + Date.now()
-    }
-    const auth = pusher.authenticate(socketId, channel, presenceData);
-    res.send(auth);
-
-});
 
 module.exports = router;
