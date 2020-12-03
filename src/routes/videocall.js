d4f4bbbeb641387cd3528af7b0d0e6a9ed5980db
Daniel Cifuentes <54119708+DanielCifuentes05@users.noreply.github.com>
2020-12-02 23:47:22 -0500

    Mejora en Videollamada
    Mejora en Videollamada con jisti meet y mensaje de alerta

--- src/routes/videocall.js
+++ src/routes/videocall.js
@@ -3,10 +3,29 @@ const router = express.Router();
 const Pusher = require('pusher');
 const {isLoggedIn} = require('../lib/auth');
 
+
 router.get('/videocall' , isLoggedIn, (req, res) => {
     res.render('videocall/videocall');
 });
 
+router.post('/videocall/auth', (req, res) => {
 
+    const pusher = new Pusher({
+        appId: '1089719',
+        key: 'aa627cb1dc3099475e48',
+        secret: 'a2d65af256706949f73b',
+        cluster: 'us2',
+        useTLS: true
+    });
+
+    const socketId = req.body.socket_id;
+    const channel = req.body.channel_name;
+    var presenceData = {
+        user_id: req.user.nombre + "-" + Date.now()
+    }
+    const auth = pusher.authenticate(socketId, channel, presenceData);
+    res.send(auth);
+
+});
 
 module.exports = router;
