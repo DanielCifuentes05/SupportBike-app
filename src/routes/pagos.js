d15a92b44fdc82666b2cf88b8763814ea22e600c
Daniel Cifuentes <54119708+DanielCifuentes05@users.noreply.github.com>
2020-12-08 00:16:31 -0500

    Cambio de MP a PayPal
    Cambio de MercadoPago a PayPal

--- src/routes/pagos.js
+++ src/routes/pagos.js
@@ -1,15 +1,16 @@
 const express = require('express');
 // SDK de Mercado Pago
-const mercadopago = require('mercadopago');
+
+var paypal = require('paypal-rest-sdk');
 const router = express.Router();
 
-const PaymentController = require("../controllers/PaymentController");
- //importamos el controller
 
-const PaymentService = require("../services/PaymentService"); 
-//importamos el service
 
-const PaymentInstance = new PaymentController(new PaymentService());
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live 
+    'client_id': 'AQb9w_UWmzOl-6WO-Js0kpItn7NBoytmBoSIyoiCIbVuHNsLN3x2BgIDlvS2D5aW38cqGaHT8vkzkkrc', // please provide your client id here 
+    'client_secret': 'ELtzVYjOPyYpFHsp-1Mf0K11dqp32k7mugexI4NOwNMFqQ1DQu5BrVNCFcs0uOGgJo2NarlANdlzMkyK' // provide your client secret here 
+  });
 
 
 router.get('/suscripciones', (req, res) => {
@@ -18,8 +19,45 @@ router.get('/suscripciones', (req, res) => {
     
 });
 
-router.post('/payment/new', (req, res) => 
-    PaymentInstance.getMercadoPagoLink(req, res));
+router.post('/payment/new', (req, res) => {
+    
+    var payment = {
+        "intent": "authorize",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "https://supportbike.herokuapp.com/success",
+            "cancel_url": "https://supportbike.herokuapp.com/failure"
+        },
+        "transactions": [{
+            "amount": {
+                "total": 39.00,
+                "currency": "USD"
+            },
+            "description": " a book on mean stack "
+        }]
+        };
+
+
+    // call the create Pay method 
+    createPay( payment ) 
+        .then( ( transaction ) => {
+            var id = transaction.id; 
+            var links = transaction.links;
+            var counter = links.length; 
+            while( counter -- ) {
+                if ( links[counter].method == 'REDIRECT') {
+                    // redirect to paypal where user approves the transaction 
+                    return res.redirect( links[counter].href )
+                }
+            }
+        })
+        .catch( ( err ) => { 
+            console.log( err ); 
+            res.redirect('/err');
+        });
+    });
 
 router.post('/webhook', (req, res) => 
     PaymentInstance.webhook(req, res));
@@ -42,5 +80,18 @@ router.get('/pending', (req, res) => {
     res.send('Pago en proceso');
     
 });
+
+var createPay = ( payment ) => {
+    return new Promise( ( resolve , reject ) => {
+        paypal.payment.create( payment , function( err , payment ) {
+         if ( err ) {
+             reject(err); 
+         }
+        else {
+            resolve(payment); 
+        }
+        }); 
+    });
+}
 
 module.exports = router;
